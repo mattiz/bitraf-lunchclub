1bde87e11e0392d933db089e221ee70f44dbe71b
Mathias Bjerke <mathbje@gmail.com>
2018-11-02 19:23:45 +0100

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!',
+        people: [
+            {
+                name: 'Mariann Orvedal',
+                photo: 'photos/1.jpg'
+            },
+            {
+                name: 'Eskil Bjerke',
+                photo: 'photos/2.jpg'
+            },
+            {
+                name: 'Marius Nettum',
+                photo: 'photos/3.jpg'
+            }
+        ]
+    }
+})
