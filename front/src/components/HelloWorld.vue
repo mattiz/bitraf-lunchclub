155595744b7d057f9c0576ef8b6181fec19fef88
Daniel Berg <mail@roosta.sh>
2018-11-15 20:52:55 +0100

    WIP: Adapt design to vue-cli

--- front/src/components/HelloWorld.vue
+++ front/src/components/HelloWorld.vue
@@ -1,38 +1,67 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+
+        <ul class="list-group">
+          <li v-bind:key="p.name" v-for="p in people" class="list-group-item">
+
+            <div class="row">
+              <div class="col-2">
+                <img v-bind:src="p.photo" class="img-thumbnail rounded-circle">
+              </div>
+
+              <div class="col-7">
+                <div class="h4 mt-2">{{ p.name }}</div>
+                <p>Antall lunsjer i november: <strong>67</strong></p>
+              </div>
+
+              <div class="col-3 d-flex align-items-center justify-content-center">
+                <i class="fas fa-utensils fa-2x"></i>
+
+
+                <!--
+                  <div class="btn-group btn-group-lg" role="group">
+                  <button type="button" class="btn">
+                  <i class="fas fa-minus"></i>
+                  </button>
+                  <input type="text" value="1" class="text-center" style="width: 50px;">
+                  <button type="button" class="btn">
+                  <i class="fas fa-plus"></i>
+                  </button>
+                  </div>
+                -->
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
+  data: function () {
+    return {
+      message: 'Hello Vue!',
+      people: [
+        {
+          name: 'Mariann Orvedal',
+          photo: 'assets/1.jpg'
+        },
+        {
+          name: 'Eskil Bjerke',
+          photo: 'assets/2.jpg'
+        },
+        {
+          name: 'Marius Nettum',
+          photo: 'assets/3.jpg'
+        }
+      ]
+    }
+  },
   props: {
     msg: String
   }
@@ -41,18 +70,30 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+
+body {
+  background-color: slategray;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.list-group-item {
+  margin-bottom: 7px;
 }
-li {
+
+.eaten {
+  /*
+  width: 50px;
+  height: 50px;
+   */
+border: 4px solid gray;
+border-radius: 25px;
+}
+
+i.fas {
+  color: #c0c0c0;
   display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+  border: 7px solid #c0c0c0;
+  border-radius: 60px;
+  box-shadow: 0px 0px 2px #c0c0c0;
+  padding: 0.3em 0.4em;
 }
 </style>
